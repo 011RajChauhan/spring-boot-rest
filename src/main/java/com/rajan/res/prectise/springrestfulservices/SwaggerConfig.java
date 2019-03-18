9235d62bc39ef1a699be80efa76a121a1251ed33
Rajan Chauhan <rajan.ch@hcl.com>
2019-03-18 21:52:02 +0530

    swagger implementation part 2

--- src/main/java/com/rajan/res/prectise/springrestfulservices/SwaggerConfig.java
+++ src/main/java/com/rajan/res/prectise/springrestfulservices/SwaggerConfig.java
@@ -2,20 +2,33 @@ package com.rajan.res.prectise.springrestfulservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
-
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    public static final Contact DEFAULT_CONTACT = new Contact("Rajan Chauhan", "011rajchauhan@gmail.com", "rajan.ch@hcl.com");
+    public static final ApiInfo DEFAULT_API_INFO =
+    new ApiInfo("Restful service API documentation", "this is a simple documentation", "1.0",
+            "urn:please do not copy paste just learn", DEFAULT_CONTACT,
+            "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList());
+    public static final java.util.Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json","application/xsml"));
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
-               /* .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();*/
+        Docket consumes = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+        return consumes;
     }
 }
--- src/main/java/com/rajan/res/prectise/springrestfulservices/users/User.java
+++ src/main/java/com/rajan/res/prectise/springrestfulservices/users/User.java
@@ -1,10 +1,15 @@
 package com.rajan.res.prectise.springrestfulservices.users;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "all details about user")
 public class User {
 
 
@@ -12,10 +17,12 @@ public class User {
 
     @Size(min=2, message = "name should at least contain two characters.")
     @NotNull(message = "name must is required")
+    @ApiModelProperty(notes = "name should be more than 2 characters")
     private String name;
 
     @Past(message = "date of birth cannot be future date.")
     @NotNull(message = "date is required")
+    @ApiModelProperty(notes = "birthdate should be less then current time")
     private Date dob;
 
     public User(String name, int id, Date dob) {
